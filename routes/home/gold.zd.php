6e9497480046689fe175941b317236ae6fcf1be8
jiguorong <jiguorong@qq.com>
2017-07-04 22:31:31 +0800

    jgr0704

--- routes/home/gold.zd.php
+++ routes/home/gold.zd.php
@@ -4,6 +4,14 @@
 *@Use : 贵金属列表页
 */
 Route::get('gold/gold', 'Home\GoldController@index');
+
 Route::get('gold/getlist', 'Home\GoldController@getlist');
+//主页贵金属展示页
 Route::get('gold/getgo', 'Home\GoldController@getgo');
+//fund
+Route::get('gold/fund', 'Home\GoldController@fund');
+//基金更多展示页面
+Route::get('gold/fundlist', 'Home\FundController@fundlist');
+
+
 
