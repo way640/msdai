f7a5c122c275d0c888cd729e57c4985cfc4c5fcb
chengye <1191326864@qq.com>
2017-07-12 20:11:40 +0800

    way

--- public/admin/css/test.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by chengye on 2017/6/11.
- */
-window.loaded = function (){
-    document.write('445454545')
-}
--- public/admin/js/main.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by chengye on 2017/6/12.
- */
-$(function ( ) {
-
-});
